518143bd0c6dfd58bf207d90ff959b45525c17b7
htdong <ht.dong@yahoo.com.vn>
2018-06-13 23:07:49 +0700

    0008

--- _services/dbConnect.service.js
+++ _services/dbConnect.service.js
@@ -30,7 +30,7 @@ var DBConnect = {
     try {
       console.log('MONGO_SYSTEM_URI %s', process.env.MONGO_SYSTEM_URI);
 
-      const systemDb = await mongoose.createConnection(process.env.MONGO_SYSTEM_URI);
+      const systemDb = await mongoose.createConnection(process.env.MONGO_SYSTEM_URI, { poolSize: 4 });
       ,
       // { promiseLibrary: require("bluebird") }
       return systemDb.model(model, schema);
